1b272dd897d9d156207893fb0175a2b231d0fdd2
takuto kakeda <takuto.kakeda@gmail.com>
2025-02-08 05:00:52 -0800

    Add useLayoutEffect polyfill for SSR

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,11 @@ import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
 import theme from './theme'
 
+// Polyfill for useLayoutEffect in SSR environments
+if (typeof window === 'undefined') {
+  React.useLayoutEffect = React.useEffect
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
