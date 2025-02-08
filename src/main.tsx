c02dd6beb348d2ddfa7fc91ee21f02378dac43d1
takuto kakeda <takuto.kakeda@gmail.com>
2025-02-08 05:09:12 -0800

    Robust useLayoutEffect polyfill and patching

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useLayoutEffect as reactUseLayoutEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
@@ -7,9 +7,28 @@ import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
 import theme from './theme'
 
-// Polyfill for useLayoutEffect in SSR environments
+// Robust useLayoutEffect polyfill
+const useLayoutEffect = typeof window !== 'undefined' 
+  ? reactUseLayoutEffect 
+  : React.useEffect;
+
+// Patch React and global object
 if (typeof window === 'undefined') {
-  React.useLayoutEffect = React.useEffect
+  (React as any).useLayoutEffect = useLayoutEffect;
+  global.React = {
+    ...global.React,
+    useLayoutEffect: useLayoutEffect
+  };
+}
+
+// Patch use-callback-ref if possible
+try {
+  const useCallbackRef = require('use-callback-ref');
+  if (useCallbackRef && typeof useCallbackRef.useLayoutEffect === 'undefined') {
+    useCallbackRef.useLayoutEffect = useLayoutEffect;
+  }
+} catch (e) {
+  console.warn('Could not patch use-callback-ref', e);
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
